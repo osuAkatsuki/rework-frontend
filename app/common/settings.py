d007ad001546db659a3ea76973ef3f8ace1c2453
cmyui <cmyuiosu@gmail.com>
2023-11-10 21:36:06 -0500

    replace API_PORT with PERFORMANCE_SERVICE_BASE_URL

--- app/common/settings.py
+++ app/common/settings.py
@@ -15,10 +15,9 @@ APP_ENV = os.environ["APP_ENV"]
 APP_COMPONENT = os.environ["APP_COMPONENT"]
 APP_HOST = os.environ["APP_HOST"]
 APP_PORT = int(os.environ["APP_PORT"])
-API_PORT = int(os.environ["API_PORT"])
 
 CODE_HOTRELOAD = read_bool(os.environ["CODE_HOTRELOAD"])
-
 SERVICE_READINESS_TIMEOUT = int(os.environ["SERVICE_READINESS_TIMEOUT"])
-
 PULL_SECRETS_FROM_VAULT = read_bool(os.environ["PULL_SECRETS_FROM_VAULT"])
+
+PERFORMANCE_SERVICE_BASE_URL = os.environ["PERFORMANCE_SERVICE_BASE_URL"]
