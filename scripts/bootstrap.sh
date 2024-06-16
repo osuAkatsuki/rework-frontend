a8e60667eb3da70ae205027aaf2d3cae32de1967
Josh Smith <cmyuiosu@gmail.com>
2024-06-16 00:21:50 -0400

    Switch vault to use $APP_ENV

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -14,8 +14,7 @@ if [ -z "$APP_COMPONENT" ]; then
 fi
 
 if [[ $PULL_SECRETS_FROM_VAULT -eq 1 ]]; then
-  # TODO: revert to $APP_ENV
-  akatsuki vault get rework-frontend production-k8s -o .env
+  akatsuki vault get rework-frontend $APP_ENV -o .env
   source .env
 fi
 
